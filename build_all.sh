48b61e78a3cd0f9c63fd775e577c93c30bb076e6
xuanhien070594@gmail.com <xuanhien070594@gmail.com>
2024-11-03 23:06:58 -0500

    Modify docker build files to adapt to new application

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,8 @@
 DIR=$(pwd)
+HOME=/home/trifinger
 
 cd $DIR/mycpp/ && mkdir -p build && cd build && cmake .. -DPYTHON_EXECUTABLE=$(which python) && make -j11
-cd /kaolin && rm -rf build *egg* && pip install -e .
+cd $HOME/kaolin && rm -rf build *egg* && IGNORE_TORCH_VER=1 pip install -e .
 cd $DIR/bundlesdf/mycuda && rm -rf build *egg* && pip install -e .
 
 cd ${DIR}
